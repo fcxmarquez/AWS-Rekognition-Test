55bed41487f8f0bcfe2f106cf4703069d2e9c6a1
fcxmarquez <francisco.marquez.solt@gmail.com>
2023-01-09 17:25:19 -0600

    App config

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const slsw = require('serverless-webpack');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: slsw.lib.entries,
+  output: {
+    libraryTarget: 'commonjs2',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
+  },
+  target: 'node',
+  externals: [nodeExternals()],
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production'
+};
